d5b6d8e2a6b2857c839f4e25cb8f9e7f1ef771f0
Pankaj Kumar <pankajkumaryadav1997@gmail.com>
2019-07-18 02:04:56 +0530

    Data population code running (except rule population)

--- populate_db/parse_prowler_csv.py
+++ populate_db/parse_prowler_csv.py
@@ -8,5 +8,27 @@ csv_file = sys.argv[1]
 
 csv_contents = pandas.read_csv(csv_file)
 
-print(csv_contents)
+#print(csv_contents)
 
+class compliance_run_result(object):
+    def __init__(self, rule_id, message, result, provider, region):
+        self.message = message
+        self.result = result
+        self.rule_id = rule_id
+        self.provider = provider
+        self.region = region
+        self.entity = "DUMMY"
+
+
+def insert_rule_result(db, result):
+    sql = "INSERT INTO compliance_run_result (rule_id, result, message, provider, region, entity) values (%s, %s, %s, %s, %s, %s)"
+    val = (result.rule_id, result.result, result.message, result.provider, result.region, result.entity)
+    print(sql, val)
+    db.cursor().execute(sql, val)
+    db.commit()
+
+
+
+for index, row in csv_contents.iterrows():
+    result = compliance_run_result(row["TITLE_ID"], row["NOTES"], row["RESULT"], "AWS", row["REGION"])
+    insert_rule_result(db, result)
