fcccf68df9a6d73152e9e3a87727734a71a40ae1
Pankaj Kumar <pankajkumaryadav1997@gmail.com>
2019-07-18 14:07:17 +0530

    Table population tested on partial

--- populate_db/parse_prowler_csv.py
+++ populate_db/parse_prowler_csv.py
@@ -2,16 +2,26 @@ import docker_database as database
 import pandas
 import sys
 import numpy
+import subprocess
+import os
 
 db = database.get_db()
 csv_file = sys.argv[1]
+group_dir = sys.argv[2]
 
 csv_contents = pandas.read_csv(csv_file)
 
-#print(csv_contents)
+
+class group(object):
+    def __init__(self, id, title, checks):
+        self.id = id
+        self.title = title.split("-")[0].strip()
+        self.checks = set(checks.split(","))
+    def __str__(self):
+        return "{id:'%s', title:'%s'}" % (self.id, self.title)
+
 
 class compliance_run_result(object):
-    
     
     def __init__(self, rule_id, message, result, provider, region):
         self.message = message
@@ -31,7 +41,7 @@ class rule(object):
         rule.map_to_id[tid] = x
         return x
         
-    def __init__(self, id, title, level):
+    def __init__(self, id, title, level, groups):
         self.severity = level
         
         index = title.find("[")
@@ -45,14 +55,13 @@ class rule(object):
         self.name = name
         self.provider = "AWS"
         self.entity_type = "DUMMY_TYPE"
-        self.group = int(float(id))
+        self.group = ",".join(filter_group_ids(groups, self.name))
         self.descritpion = description
         if level == "Support":
             return
         self.id = rule.get_id(id)
     
     def __str__(self):
-        
         return "%s -- %s -- %s" % (self.name, self.descritpion, self.severity)
     
     def persist(self, db):
@@ -69,15 +78,14 @@ class rule(object):
 def insert_rule_result(db, result):
     sql = "INSERT INTO compliance_run_result (rule_id, result, message, provider, region, entity) values (%s, %s, %s, %s, %s, %s)"
     val = (result.rule_id, result.result, result.message, result.provider, result.region, result.entity)
-    print(sql, val)
     db.cursor().execute(sql, val)
     db.commit()
 
 
-def get_rules(csv):
+def get_rules(csv, groups):
     csv.sort_values(['TITLE_ID'])
     grp = csv.groupby("TITLE_ID")["TITLE_TEXT", "LEVEL"].apply(lambda obj: obj.loc[numpy.random.choice(obj.index), :])
-    t = grp.apply(lambda row: rule(row.name, row["TITLE_TEXT"], row["LEVEL"]), axis=1)
+    t = grp.apply(lambda row: rule(row.name, row["TITLE_TEXT"], row["LEVEL"], groups), axis=1)
     return t
 
 
@@ -88,9 +96,40 @@ def insert_all(csv_contents):
             insert_rule_result(db, result)
 
 def insert_rules(rules, db):
-    print(type(rules), rules)
     [x.persist(db) for x in rules.tolist()]
+
+def parse_group(filename):
+    out = subprocess.check_output("cat " +filename+ " | sed -e 's/#.*$//' -e '/^$/d' -e 's/\[[^][]*\]//g'", shell=True).decode("utf8")
+    lines = out.splitlines()
+    m = {}
+    for line in lines:
+        index = line.find("=")
+        if index != -1:
+            key = line[:index]
+            value = line[index+1:].replace("'", "")
+            m[key] = value
+    g = group(m["GROUP_ID"], m["GROUP_TITLE"], m["GROUP_CHECKS"])
+    return g.id, g
+
+
+def get_all_groups(directory):
+    result = {}
+    for filename in os.listdir(directory):
+        id, g = parse_group(os.path.join(directory, filename))
+        result[id] = g
+    return result
     
 
-insert_rules(get_rules(csv_contents), db)
+def filter_group_ids(groups, ruleName):
+    result = []
+    for g in groups.values():
+        if ruleName in g.checks:
+            result.append(g.title)
+    return set(result)
+
+
+groups = get_all_groups(group_dir)
+print(groups)
+insert_rules(get_rules(csv_contents, groups), db)
 insert_all(csv_contents)
+
