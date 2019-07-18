12581c9b065eff4116f4495790e3226c76c4915c
arvind.batra <arvind.batra@rubrik.com>
2019-07-18 10:28:59 +0530

    fixed tmp2

--- populate_db/tmp2.sh
+++ populate_db/tmp2.sh
@@ -1,4 +1,4 @@
-insert into compliance_rule_results(rule_id, provider, region, entity, result,
-message, timestamp) values (1, 'aws', 'us-west-2','iam', 'PASS','anant.mahajan has Password enabled but MFA disabled', '2019-06-07T22');
-insert into compliance_rule_results(rule_id, provider, region, entity, result,
-message, timestamp) values (1, 'aws', 'us-west-2','iam', 'FAIL','anant.mahajan has Password enabled but MFA disabled', '2019-06-07T22');
+insert into compliance_rule_results(rule_id, provider, region,  result,
+message, timestamp) values (1, 'aws', 'us-west-2', 'PASS','anant.mahajan has Password enabled but MFA disabled', '2019-06-07T22');
+insert into compliance_rule_results(rule_id, provider, region, result,
+message, timestamp) values (1, 'aws', 'us-west-2', 'FAIL','anant.mahajan has Password enabled but MFA disabled', '2019-06-07T22');
